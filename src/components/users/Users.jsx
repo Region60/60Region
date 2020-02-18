ee7bbd1f491b3042731676ecd215a438706ac33d
Region60 <_max_kot@mail.ru>
2020-02-19 00:19:14 +0300

    Lesson_62_follow/unfollow_delete/post

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -28,18 +28,35 @@ let Users = (props) => {
                 <div>
                     <div>
                         <NavLink to={'/profile/' + u.id}>
-                        <img src={u.photos.small != null ? u.photos : userImg} className={classes.photo}/>
+                            <img src={u.photos.small != null ? u.photos.small : userImg} className={classes.photo}/>
                         </NavLink>
                     </div>
                     <div>
-                        {u.followed
-                            ? <button onClick={() => {
-                                props.follow(u.id)
+                        {u.followed ?
+                            <button onClick={() => {
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,{
+                                    withCredentials: true,
+                                    "API-KEY": "ae9f4f72-b180-4d04-a84e-9fd4f03c3b27"
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    })
+
                             }}>Unfollow</button>
-                            : <button onClick={() => {
-                                props.unfollow(u.id)
-                            }}>Follow</button>
-                        }
+                            :
+                            <button onClick={() => {
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                    withCredentials: true,
+                                    "API-KEY": "ae9f4f72-b180-4d04-a84e-9fd4f03c3b27"
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.follow(u.id)
+                                        }
+                                    })
+                            }}>Follow</button>}
                     </div>
                 </div>
                 <div>
