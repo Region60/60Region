14101e708ddf6c295ba52e86d7ae995f697e3d32
Region60 <_max_kot@mail.ru>
2020-03-21 16:13:03 +0300

    Lesson_77_redux-form_part_2

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import classes from './FormsControls.module.css';
 
-export const Textarea = ({input, meta, ...props}) => { //реструкторизация rest оператор
+export const FormControl = ({input, meta, ...props}) => { //реструкторизация rest оператор
     const hasError = meta.touched && meta.error;
     return (
         <div className={classes.formControl + "" + (hasError ? classes.error : "")}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 };
 
-export const Input = ({input, meta, ...props}) => { //реструкторизация rest оператор
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={classes.formControl + "" + (hasError ? classes.error : "")}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
-};
+export const Textarea = (props) => {
+    const {input, meta, child, ...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...props}/></FormControl>
+}
+
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props
+    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
+}
