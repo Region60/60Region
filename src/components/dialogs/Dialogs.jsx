1b9b5a6fdd68721eccbacdea571c90c7fd81b1b3
Region60 <_max_kot@mail.ru>
2020-05-08 22:50:29 +0300

    'home__'

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -39,13 +39,15 @@ const Dialogs = (props) => {
         </div>
     )
 }
+
 const maxLength50 = maxLengthCreator(50)
+
 const AddMessageForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field component={Textarea} validate={[required, maxLength50]} name='newMessageText'
-                       placeholder='Enter your message'/>
+                       placeholder='Enter your message' />
             </div>
 
             <div>
